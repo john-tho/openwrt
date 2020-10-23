581601a5ea44f386307303d262c73bd52a2dec9c
John Thomson <git@johnthomson.fastmail.com.au>
2020-10-24 09:32:39 +1000

    ipq40xx: Add Mikrotik RouterBoard RB450Gx4 support
    RB450Gx4 is a 5 port gigabit router based on IPQ4019 + QCA8075.
    
    Specification:
    
    - Qualcomm IPQ4019 (717 MHz)
    - 1024 MB of RAM (DDR3L)
    - 2 MB (SPI NOR) + 512 MB (Parallel NAND) of flash
    - 5x Gbit ethernet, PoE IN on port 1 and PoE passtrough to port 5.
    - No WLAN radios enabled
    - No USB ports
    - micro SD card slot
    - DB9 connector with UART on 115200 baud
    - 2x LEDs
    - GPIO Beeper
    - 2x DC input
    
    Due to PoE out passthrough on QCA8075 port5,
    port1 as WAN (atypical IPQ4019 device)
    
    OEM MAC addrs:
    - switch 1: label_mac
    - switch 2: label_mac +1
    - switch 3: label_mac +2
    - switch 4: label_mac +3
    - switch 5: label_mac +4
    
    Match vendor UBI MTD parition sizes
    
    Signed-off-by: Robert Marko <robimarko@gmail.com>
    [rebase to hap-ac2 merged to master, port1->WAN, port5->LAN,
    NAND aware sysupgrade]
    Signed-off-by: John Thomson <git@johnthomson.fastmail.com.au>

--- target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
+++ target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
@@ -25,6 +25,11 @@ Once this is done. Retry.
 EOF
 		return 1
 		;;
+	mikrotik,rb450gx4*)
+		mikrotik_check_nand_blocksize "$1"
+		return $?
+		;;
+
 	zte,mf286d)
 		CI_UBIPART="rootfs"
 		local mtdnum="$( find_mtd_index $CI_UBIPART )"
@@ -176,7 +181,9 @@ platform_do_upgrade() {
 		[ "$(rootfs_type)" = "tmpfs" ] && mtd erase firmware
 		default_do_upgrade "$1"
 		;;
-	mikrotik,hap-ac3)
+	mikrotik,hap-ac3|\
+	mikrotik,rb450gx4*)
+		mikrotik_rm_nand_routeros && \
 		platform_do_upgrade_mikrotik_nand "$1"
 		;;
 	netgear,rbr50 |\
