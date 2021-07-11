7cacefdf7d6a69bbf6e9d8ca3af5e7117b9db75f
John Thomson <git@johnthomson.fastmail.com.au>
2021-07-12 06:09:47 +1000

    ramips: mikrotik hexs (760igs) add temp / voltage support
    Add the kernel touchscreen driver and configuration details for the
    2046q touchscreen controller used only as a temperature and voltage sensor.
    
    The ads7846 driver requires a pendown and interrupt gpio defined,
    even through these are not needed for temperature and voltage.
    
    voltage = temp1/32
    temperature ?= temp0
    
    Signed-off-by: John Thomson <git@johnthomson.fastmail.com.au>

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -1813,7 +1813,7 @@ define Device/MikroTik
   $(Device/dsa-migration)
   DEVICE_VENDOR := MikroTik
   IMAGE_SIZE := 16128k
-  DEVICE_PACKAGES := kmod-usb3 -uboot-envtools
+  DEVICE_PACKAGES := kmod-usb3 -uboot-envtools kmod-input-touchscreen-ads7846
   KERNEL_NAME := vmlinuz
   KERNEL := kernel-bin | append-dtb-elf
   LOADER_TYPE := elf
