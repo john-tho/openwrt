b337105c29ae57237cb45db91f83b67160046f45
John Thomson <git@johnthomson.fastmail.com.au>
2022-10-03 19:02:03 +1000

    ipq40xx: mikrotik: set iface MAC address in preinit
    Signed-off-by: John Thomson <git@johnthomson.fastmail.com.au>

--- target/linux/ipq40xx/base-files/lib/preinit/05_set_iface_mac_ipq40xx.sh
+++ target/linux/ipq40xx/base-files/lib/preinit/05_set_iface_mac_ipq40xx.sh
@@ -1,5 +1,31 @@
 . /lib/functions.sh
 
+ipq40xx_mikrotik_mac_address() {
+	base_mac=$(cat /sys/firmware/mikrotik/hard_config/mac_base)
+	counter=0
+	if [ ! -d /sys/class/net/eth0/dsa ]; then
+		return
+	fi
+	ip link set dev eth0 address $(macaddr_add "$base_mac" "$counter")
+	# first port is WAN, and has mac_base MAC address
+	if [ -d /sys/class/net/eth0/upper_wan ]; then
+		iface=wan
+		ip link set dev "$iface" address $(macaddr_add "$base_mac" "$counter")
+		counter=$((counter+1))
+	fi
+	# each other port has a consecutive MAC address
+	for member in /sys/class/net/eth0/upper*; do
+		iface=${member#*_}
+		case "$iface" in
+		lan*|ether*)
+			ip link set dev "$iface" address $(macaddr_add "$base_mac" "$counter")
+			counter=$((counter+1))
+			;;
+		esac
+	done
+}
+
+
 preinit_set_mac_address() {
 	case $(board_name) in
 	asus,map-ac2200)
@@ -24,10 +50,8 @@ preinit_set_mac_address() {
 		ip link set dev lan1 address $(macaddr_add "$base_mac" 1)
 		ip link set dev eth0 address $(macaddr_setbit "$base_mac" 7)
 		;;
-	mikrotik,wap-ac)
-		base_mac=$(cat /sys/firmware/mikrotik/hard_config/mac_base)
-		ip link set dev eth0 address "$base_mac"
-		ip link set dev eth1 address $(macaddr_add "$base_mac" 1)
+	mikrotik,*)
+		ipq40xx_mikrotik_mac_address
 		;;
 	zyxel,nbg6617)
 		base_mac=$(cat /sys/class/net/eth0/address)
