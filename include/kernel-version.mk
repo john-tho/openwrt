d3e65c0420e16a93542320b472e35b1391110b3a
John Thomson <git@johnthomson.fastmail.com.au>
2022-12-16 13:08:05 +1000

    build: kernel: add sublevel 0 to linux version
    Expand OpenWrt KERNEL_PATCHVER to maintain compatability with existing
    OpenWrt build functionality of a NULL sublevel, or an -rcX sublevel
    
    tested with:
    
    cat <<'EOF'>/tmp/Makefile.test
    expand_kernel_sublevel=$(or $(if $(filter -rc%,$(1)),$(1:-rc%=.0-rc%),),$(1),.0)
    
    sublevel_tests := .15 .0 .0-rc2 -rc2
    
    test:
    	@$(foreach sublevel,$(sublevel_tests),echo "expand_kernel_sublevel($(sublevel))=$(call expand_kernel_sublevel,$(sublevel))";)
    	@echo "expand_kernel_sublevel("")=$(call expand_kernel_sublevel,)";
    EOF
    make -f /tmp/Makefile.test
    
    expand_kernel_sublevel(.15)=.15
    expand_kernel_sublevel(.0)=.0
    expand_kernel_sublevel(.0-rc2)=.0-rc2
    expand_kernel_sublevel(-rc2)=.0-rc2
    expand_kernel_sublevel()=.0
    
    Signed-off-by: John Thomson <git@johnthomson.fastmail.com.au>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -15,6 +15,7 @@ include $(KERNEL_DETAILS_FILE)
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
+expand_kernel_sublevel=$(or $(if $(filter -rc%,$(1)),$(1:-rc%=.0-rc%),),$(1),.0)
 
 ifneq ($(call qstrip,$(CONFIG_KERNEL_GIT_CLONE_URI)),)
   LINUX_VERSION:=$(call sanitize_uri,$(call remove_uri_prefix,$(CONFIG_KERNEL_GIT_CLONE_URI)))
@@ -24,10 +25,10 @@ ifneq ($(call qstrip,$(CONFIG_KERNEL_GIT_CLONE_URI)),)
   LINUX_VERSION:=$(LINUX_VERSION)-$(call sanitize_uri,$(CONFIG_KERNEL_GIT_REF))
 else
 ifdef KERNEL_PATCHVER
-  LINUX_VERSION:=$(KERNEL_PATCHVER)$(strip $(LINUX_VERSION-$(KERNEL_PATCHVER)))
+  LINUX_VERSION:=$(KERNEL_PATCHVER)$(strip $(call expand_kernel_sublevel,$(LINUX_VERSION-$(KERNEL_PATCHVER))))
 endif
 ifdef KERNEL_TESTING_PATCHVER
-  LINUX_TESTING_VERSION:=$(KERNEL_TESTING_PATCHVER)$(strip $(LINUX_VERSION-$(KERNEL_TESTING_PATCHVER)))
+  LINUX_TESTING_VERSION:=$(KERNEL_TESTING_PATCHVER)$(strip $(call expand_kernel_sublevel,$(LINUX_VERSION-$(KERNEL_TESTING_PATCHVER))))
 endif
 endif
 
@@ -38,5 +39,5 @@ KERNEL=$(call merge_version,$(wordlist 1,2,$(call split_version,$(KERNEL_BASE)))
 KERNEL_PATCHVER ?= $(KERNEL)
 
 # disable the md5sum check for unknown kernel versions
-LINUX_KERNEL_HASH:=$(LINUX_KERNEL_HASH-$(strip $(LINUX_VERSION)))
+LINUX_KERNEL_HASH:=$(LINUX_KERNEL_HASH-$(strip $(KERNEL_PATCHVER)$(LINUX_VERSION-$(KERNEL_PATCHVER))))
 LINUX_KERNEL_HASH?=x
