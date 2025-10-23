damiankaczkowski/openwrt-john-tho

ath79: mikrotik NOR: build yafut images
Signed-off-by: John Thomson <git@johnthomson.fastmail.com.au>

target/linux/ath79/image/common-mikrotik.mk
@@ -17,6 +17,12 @@ define Device/mikrotik_nor
   IMAGE/sysupgrade-v7.bin := append-kernel | kernel-pack-npk | \
 	  kernel2minor -b -s 1024 -e | pad-to $$$$(BLOCKSIZE) | \
 	  append-rootfs | pad-rootfs | check-size | append-metadata
+  IMAGES += sysupgrade-yafut.bin
+  IMAGES/sysupgrade-yafut.bin := append-kernel | sysupgrade-tar | append-metadata
+
+  IMAGES += sysupgrade-yafut-v7.bin
+  IMAGES/sysupgrade-yafut-v7.bin := append-kernel | kernel-pack-npk | \
+	  sysupgrade-tar | append-metadata
 endef
 
 define Device/mikrotik_nand

john-tho/openwrt

target/linux/ath79/image/common-mikrotik.mk
@@ -19,6 +19,17 @@ define Device/mikrotik_nor
 	  append-rootfs | pad-rootfs | check-size | append-metadata
 endef
 
+define Device/mikrotik_nor_yafut
+  $(Device/mikrotik)
+  DEVICE_PACKAGES := yafut
+  IMAGES += sysupgrade-yafut.bin
+  IMAGE/sysupgrade-yafut.bin := append-kernel | sysupgrade-tar | append-metadata
+
+  IMAGES += sysupgrade-yafut-v7.bin
+  IMAGE/sysupgrade-yafut-v7.bin := append-kernel | kernel-pack-npk | \
+	  sysupgrade-tar | append-metadata
+endef
+
 define Device/mikrotik_nand
   $(Device/mikrotik)
   IMAGE/sysupgrade.bin = append-kernel | sysupgrade-tar | append-metadata